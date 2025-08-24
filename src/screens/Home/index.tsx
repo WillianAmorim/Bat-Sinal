4c483914a9b66d443bacc2ebe1ed1487e574f529
Willian Amorim <willianamorim1@outlook.com.br>
2025-08-24 19:25:38 -0300

    adicionado segunda tela de formulário

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,16 +1,110 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { Alert, Text, View, Image, TextInput, ScrollView, Pressable } from 'react-native';
 import LogoBat from '../../../assets/logo-bat.png'
+import LogoBatMine from '../../../assets/logo-bat-small.png'
 
 import { styles } from './style';
+import { MaskedTextInput } from 'react-native-mask-text';
 
 export function Home() {
+    const [name, setName] = useState<string>('')
+    const [phone, setPhone] = useState<string>('')
+    const [localization, setLocalization] = useState<string>('')
+    const [observation, setObservation] = useState<string>('')
+
+    const [isActive, setIsActive] = useState<boolean>(false)
+
+    const handleSend = () => {
+        Alert.alert(
+            "Dados Enviados",
+            "Seus dados foram enviados com sucesso.",
+        );
+
+        setName('')
+        setPhone('')
+        setLocalization('')
+        setObservation('')
+
+    }
+
+    const handleNextPass = () => {
+        setIsActive(true)
+    }
+
     return (
         <View style={styles.container}>
-            <Image source={LogoBat}/>
-            <Text style={styles.buttonHome}>Active bat signal</Text>
-            <StatusBar style="auto" />
+            <View style={[
+                styles.containerHome,
+                { display: isActive ? "none" : "flex" }
+            ]}>
+                <Image source={LogoBat} />
+                <Pressable
+                    onPress={handleNextPass}
+                >
+                    <Text style={styles.buttonHome}>Active bat signal</Text>
+                </Pressable>
+                <StatusBar style="auto" />
+            </View>
+            <View style={[
+                styles.containerForm,
+                { display: isActive ? "flex" : "none" }
+            ]}>
+                <Image
+                    source={LogoBatMine}
+                />
+                <View style={styles.containerInput}>
+                    <Text style={styles.label}>Nome</Text>
+                    <TextInput
+                        style={styles.textInputSimple}
+                        placeholder='Digite seu nome'
+                        value={name}
+                        onChangeText={setName}
+                    />
+                </View>
+                <View style={styles.containerInput}>
+                    <Text style={styles.label}>Telefone</Text>
+                    <MaskedTextInput
+                        mask="(99) 99999-9999"
+                        onChangeText={(text, rawText) => setPhone(rawText)}
+                        keyboardType="phone-pad"
+                        placeholder="Digite seu telefone"
+                        value={phone}
+                        style={styles.textInputSimple}
+                    />
+                </View>
+                <View style={styles.containerInput}>
+                    <Text style={styles.label}>Localização atual</Text>
+                    <TextInput
+                        style={styles.textInputMultiline}
+                        placeholder="Digite sua mensagem"
+                        multiline={true}
+                        value={localization}
+                        onChangeText={setLocalization}
+                    />
+                </View>
+                <View style={styles.containerInput}>
+                    <Text style={styles.label}>Observação</Text>
+                    <TextInput
+                        style={styles.textInputMultiline}
+                        placeholder="Digite sua mensagem"
+                        multiline={true}
+                        value={observation}
+                        onChangeText={setObservation}
+                    />
+                </View>
+
+                <Pressable
+                    onPress={handleSend}
+                >
+                    <Text
+                        style={styles.buttonSend}
+                    >
+                        Enviar
+                    </Text>
+                </Pressable>
+
+            </View>
         </View>
     );
 }
--- src/screens/Home/style.tsx
+++ src/screens/Home/style.tsx
@@ -1,18 +1,68 @@
 import { StyleSheet } from 'react-native';
 
 export const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'space-evenly',
-    alignItems: 'center'
-  },
+    container: {
+        flex: 1
+    },
 
-  buttonHome: {
-    backgroundColor: '#383838',
-    paddingHorizontal: 50,
-    paddingVertical: 10,
-    color: 'white',
-    fontSize: 30,
-    borderRadius: 25
-  }
+    containerHome: {
+        justifyContent: 'space-evenly',
+        alignItems: 'center',
+        flex: 1,
+    },
+
+    buttonHome: {
+        backgroundColor: '#383838',
+        paddingHorizontal: 50,
+        paddingVertical: 10,
+        color: 'white',
+        fontSize: 30,
+        borderRadius: 25,
+    },
+
+    containerForm: {
+        paddingHorizontal: 25,
+        paddingTop: 100,
+        gap: 15
+    },
+
+    containerInput: {
+        gap: 5
+    },
+
+    label: {
+        color: '#383838',
+        fontWeight: '700',
+        fontSize: 20
+    },
+
+    textInputSimple: {
+        borderColor: '#383838',
+        borderWidth: 3,
+        borderRadius: 15,
+        textAlign: 'left',
+        paddingLeft: 20,
+        fontWeight: '500',
+        fontSize: 20
+    },
+
+    textInputMultiline: {
+        borderWidth: 3,
+        borderColor: '#383838',
+        borderRadius: 15,
+        padding: 10,
+        fontWeight: '500',
+        fontSize: 20,
+        height: 150,
+        textAlignVertical: "top",
+    },
+
+    buttonSend: {
+        backgroundColor: '#383838',
+        color: 'white',
+        fontSize: 30,
+        borderRadius: 25,
+        textAlign: 'center',
+        paddingVertical: 15
+    }
 });
